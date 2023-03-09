7aea068d09b15af32f94ddb27ee2a3eba8502f6d
Leaandro Landeo <leandrolandeo1@gmail.com>
2023-03-09 20:16:38 +0100

    firstCommit

---
+++ script.js
@@ -0,0 +1,58 @@
+
+
+
+// // numeros
+// let numero = 23;
+
+// console.log(typeof(numero));
+
+// //string
+// let string = "Leandro landeo";
+
+// console.log(typeof(string));
+
+// //boolean
+// let boolean = true; 
+// console.log(typeof(boolean));
+
+// // // undefined 
+// // let undefined
+
+// // console.log(typeof(undefined));
+
+// // //null 
+// // no tengo idea :(
+
+
+
+// //EJE 7 
+
+// let num= 5; 
+// (num+=5)
+
+// console.log(num);
+
+//EJEM8 
+let palabras = 'La importancia de la sinopsis argumental...'
+
+let twoPalabra = 'Lo más importante de la sinopsis argumental es que cumpla el propósito para el que se escribe.'
+
+let result = palabras + twoPalabra; 
+
+ console.log(result);
+// console.log(palabras + twoPalabra);
+
+//EJEM 9
+
+let n= 5; 
+
+let p=toString(n)
+
+console.log(typeof(p));
+
+
+
+
+
+
+
